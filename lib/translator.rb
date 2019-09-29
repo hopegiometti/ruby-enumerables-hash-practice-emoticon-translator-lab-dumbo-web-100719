d0c2442af1439dc7fbea10a0cea1daf7daa8fb3f
Hope Giometti <hopegiometti@gmail.com>
2019-09-29 01:18:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,10 @@
 # require modules here
-  require 'yaml'
+require 'yaml'
+
+def load_library
 
 
-def load_library(file_path)
-  file= File.read(file_path)
-  emoticons = YAML.load(file)  
+
 end
 
 def get_japanese_emoticon
