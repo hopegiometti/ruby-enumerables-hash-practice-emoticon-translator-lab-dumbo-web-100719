44998133ef22173f120275dc844b89d0207d18d2
Hope Giometti <hopegiometti@gmail.com>
2019-09-29 01:36:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,17 @@
 # require modules here
 require 'yaml'
 
-def load_library
+def load_library(path)
 
+emoticons = YAML.load_file(path)
+emoticon_hash = {}
 
+emoticon_hash["get_emoticon"] = {}
+emoticon_hash["get_meaning"] = {}
+
+emoticons.each do |definition, emoticon_set|
+  emoticon_hash["get_emoticon"][emoticon_set[0]] = emoticon_set[1]
+  emoticon_hash["get_meaning"][emoticon_set[1]] = definition
 
 end
 
@@ -14,3 +22,10 @@ end
 def get_english_meaning
   # code goes here
 end
+
+
+#get meaning --> keys are japanese emoticons 
+# get emoticon --> keys are english emoticons
+#trying to return a hash that has two keys, one is get meaning and one is get emoticon 
+#the emoticon keys inside the emoticon hash point to their japanese equivalents 
+#and vice versa 
